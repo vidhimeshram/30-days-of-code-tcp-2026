c52157d6f7d31b5f6818fbe9cd7884fdaab0d7e5
vidhimeshram <vidhimeshram7@gmail.com>
2026-01-06 19:46:15 +0530

    determine the time and space complexity.

---
+++ day-6/README.md
@@ -0,0 +1,7 @@
+# 30-days-of-code-tcp-2026
+# DAY 6 — What is the worst case time complexity of the following code:
+
+
+
+
+
---
+++ day-6/findMinPath.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[100][100];
+
+int findMinPath(vector<vector<int>> &V, int r, int c) {
+    int R = V.size();
+    int C = V[0].size();
+
+    if (r >= R || c >= C) return 100000000;
+    if (r == R - 1 && c == C - 1) return 0;
+
+    if (dp[r][c] != -1) return dp[r][c];
+
+    return dp[r][c] = V[r][c] +
+           min(findMinPath(V, r + 1, c),
+               findMinPath(V, r, c + 1));
+}
+
+int main() {
+    vector<vector<int>> V = {
+        {1, 3, 1},
+        {1, 5, 1},
+        {4, 2, 1}
+    };
+
+    memset(dp, -1, sizeof(dp));
+    cout << findMinPath(V, 0, 0);
+}
